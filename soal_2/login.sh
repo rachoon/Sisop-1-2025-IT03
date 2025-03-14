444930be46be8e440f86966cb29e9402e671bfc4
Zein Muhammad Hasan <hasro71@gmail.com>
2025-03-14 23:53:01 +0700

    Update login.sh

--- soal_2/login.sh
+++ soal_2/login.sh
@@ -3,35 +3,33 @@
 DATABASE="./data/player.csv"
 STATIC_SALT="Salt123"  # Static salt yang sama seperti di register.sh
 
-# parameter login
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <email> <password>"
-    exit 1
-fi
+# Meminta input dari pengguna
+echo -n "Masukkan email: "
+read EMAIL
+echo -n "Masukkan password: "
+read -s PASSWORD  # Input password secara tersembunyi
+echo ""
 
-EMAIL="$1"
-PASSWORD="$2"
-
-# validasi email
+# Validasi email
 if ! [[ "$EMAIL" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
     echo "Error: Format email tidak valid!"
     exit 1
 fi
 
-# mengecek data email di database
+# Mengecek data email di database
 USER_DATA=$(grep "^$EMAIL," "$DATABASE")
 if [ -z "$USER_DATA" ]; then
     echo "Error: Email tidak terdaftar!"
     exit 1
 fi
 
-# hashing passwordd dari database
+# Mengambil password yang tersimpan di database
 STORED_PASSWORD=$(echo "$USER_DATA" | cut -d',' -f3)
 
-# hasing ulang password yang diinput untuk dibandingkan dengan data yang ada di database
+# Hashing ulang password yang diinput untuk dibandingkan dengan data yang ada di database
 HASHED_INPUT_PASSWORD=$(echo -n "$PASSWORD$STATIC_SALT" | sha256sum | awk '{print $1}')
 
-# mencocokan hashing dari password
+# Mencocokkan hashing dari password
 if [ "$HASHED_INPUT_PASSWORD" != "$STORED_PASSWORD" ]; then
     echo "Error: Password salah!"
     exit 1
