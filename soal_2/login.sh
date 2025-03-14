b53fb3cf93d9e3931286ae446c8b02e9bb5ca877
Afriza Tristan C. R. <afinrajasa@gmail.com>
2025-03-14 10:47:04 +0700

    login.sh

--- soal_2/login.sh
+++ soal_2/login.sh
@@ -3,7 +3,7 @@
 DATABASE="./data/player.csv"
 STATIC_SALT="Salt123"  # Static salt yang sama seperti di register.sh
 
-# Pastikan parameter lengkap
+# parameter login
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <email> <password>"
     exit 1
@@ -12,26 +12,26 @@ fi
 EMAIL="$1"
 PASSWORD="$2"
 
-# Validasi format email
+# validasi email
 if ! [[ "$EMAIL" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
     echo "Error: Format email tidak valid!"
     exit 1
 fi
 
-# Cek apakah email ada di database
+# mengecek data email di database
 USER_DATA=$(grep "^$EMAIL," "$DATABASE")
 if [ -z "$USER_DATA" ]; then
     echo "Error: Email tidak terdaftar!"
     exit 1
 fi
 
-# Ambil hash password dari database
+# hashing passwordd dari database
 STORED_PASSWORD=$(echo "$USER_DATA" | cut -d',' -f3)
 
-# Hash ulang password yang diinput untuk dibandingkan
+# hasing ulang password yang diinput untuk dibandingkan dengan data yang ada di database
 HASHED_INPUT_PASSWORD=$(echo -n "$PASSWORD$STATIC_SALT" | sha256sum | awk '{print $1}')
 
-# Cocokkan hash password
+# mencocokan hashing dari password
 if [ "$HASHED_INPUT_PASSWORD" != "$STORED_PASSWORD" ]; then
     echo "Error: Password salah!"
     exit 1
